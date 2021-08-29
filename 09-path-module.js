ebc0bc691e625e72b2e0c1e2381561d44f48943f
OM Miles <miles@ommiles.com>
2021-08-29 16:02:58 -0700

    change dir name

--- 09-path-module.js
+++ 09-path-module.js
@@ -2,11 +2,11 @@ const path = require('path');
 console.log(path.sep);
 console.log(path);
 
-const filePath = path.join('/content', 'subdir', 'text.txt');
+const filePath = path.join('/00-content', 'subdir', 'text.txt');
 console.log(filePath);
 
 const base = path.basename(filePath);
 console.log(base);
 
-const absolute = path.resolve(__dirname, 'content', 'subdir', 'test.txt');
+const absolute = path.resolve(__dirname, '00-content', 'subdir', 'test.txt');
 console.log(absolute);
--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,19 +1,19 @@
 const { readFile, writeFile } = require('fs');
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
+readFile('./00-content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err);
     return;
   }
   const first = result;
-  readFile('./content/second.txt', 'utf8', (err, result) => {
+  readFile('./00-content/second.txt', 'utf8', (err, result) => {
     if (err) {
       console.log(err);
       return;
     }
     const second = result;
     writeFile(
-      './content/fourth.txt',
+      './00-content/fourth.txt',
       `FIRST FILE TEXT: ${first}..  SECOND FILE TEXT: ${second}..`,
       (err, result) => {
         if (err) {
