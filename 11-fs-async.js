8ea25608e257b567bf4894e84b9f008b3ac3eea3
OM Miles <miles@ommiles.com>
2021-08-29 13:56:22 -0700

    fs module asynchronous

---
+++ 11-fs-async.js
@@ -0,0 +1,27 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      './content/fourth.txt',
+      `FIRST FILE TEXT: ${first}..  SECOND FILE TEXT: ${second}..`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
