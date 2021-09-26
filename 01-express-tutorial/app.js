611efca2a41ee50de8379461b3818ba775711581
O.M. Miles <miles@ommiles.com>
2021-09-25 18:54:40 -0700

    set up basic json

--- 01-express-tutorial/app.js
+++ 01-express-tutorial/app.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const path = require('path');
+const { products } = require('./data');
 
 const app = express();
 
-app.use(express.static('./public'));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Page Not Found!</h1>');
+  res.json(products);
 });
 
 app.listen(5000, () => {
